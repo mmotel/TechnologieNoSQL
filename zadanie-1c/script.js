b383dfb2de660212ae10b56e74dcb41c20bc91e4
mmotel <mateuszmotel@gmail.com>
2013-10-21 15:46:24 +0200

    dodałem wypisywanie kontrolne

--- zadanie-1c/script.js
+++ zadanie-1c/script.js
@@ -3,75 +3,82 @@ var mongo = require('mongodb');
 var db = new mongo.Db('train', new mongo.Server('localhost', 27017), {safe: true});
 
 db.open(function (err) {
-  if(err){ console.log(err); }
-  else{
-    console.log('MongoDB Połączono!');
+	if(err){ console.log(err); }
+	else{
+		console.log('MongoDB Połączono!');
 
-    db.collection('test', function (err, coll) {
-      if(err){
-        db.close();
-        console.log(err); 
-      }
-      else{
-        var cursor = coll.find();
-        var tagsCount = 0;
-        var itemsCount = 0;
-        var updatesCount = 0;
-        var updatedCount = 0;
-        var tags = {};
-        var diffTags = 0;
+		db.collection('train', function (err, coll) {
+			if(err){
+				db.close();
+				console.log(err); 
+			}
+			else{
+				var cursor = coll.find();
+				var tagsCount = 0;
+				var itemsCount = 0;
+				var updatesCount = 0;
+				var updatedCount = 0;
+				var tags = {};
+				var diffTags = 0;
 
-        cursor.each(function(err, item) {
-          if(err){
-            db.close();
-            console.log(err); 
-          }
-          else if(item === null){
-              //czekamy aż mongo zakończy update-y
-              var interval = setInterval( function(){
-                if(updatesCount !== updatedCount){
-                  console.log("Czekam na wszystkie update-y...");
-                }
-                else{
-                  clearInterval(interval);
-                  db.close();
-                  console.log("Update-y zakończone.");
-                  console.log('MongoDB Rozłączone!');
-                  console.log("ilość obiektów: " + itemsCount);
-                  console.log("ilość updateów: " + updatesCount);
-                  console.log("   ilość tagów: " + tagsCount);
-                  console.log(" różnych tagów: " + diffTags);
-                }
-              }, 500);
-          }
-          else{
-            itemsCount++;
-            if(item.Tags.constructor === String){
-              // console.log("id: " + item.Id + " tags: " + item.Tags );
-              //rozdzielamy string do tablicy
-              var tagsSplited = item.Tags.split(" "); 
-              tagsCount += tagsSplited.length;
-              //zliczanie różnych tagów
-              for(var i=0; i < tagsSplited.length; i++){
-                if(tags[tagsSplited[i]] === undefined){
-                  tags[tagsSplited[i]] = 1; //cokolwiek byle pole było defined
-                  diffTags++;
-                }else{
-                  tags[tagsSplited[i]]++;
-                }
-              }
-              //zamiana stringa na tablicę w bazie
-              coll.update({Id: item.Id}, {$set: {Tags: tagsSplited}}, function(err){
-                if(err) { console.log(err); }
-                else{
-                  updatedCount++; //liczymy wykonane update-y
-                }
-              });
-              updatesCount++; //liczymy ilość update-ów do wykonania
-            }
-          }
-        });
-      }
-    });
-  }
+				cursor.each(function(err, item) {
+					if(err){
+						db.close();
+						console.log(err); 
+					}
+					else if(item === null){
+							//czekamy aż mongo zakończy update-y
+							var interval = setInterval( function(){
+								if(updatesCount !== updatedCount){
+									console.log("Czekam na wszystkie update-y...");
+								}
+								else{
+									clearInterval(interval);
+									db.close();
+									console.log("Update-y zakończone.");
+									console.log('MongoDB Rozłączone!');
+									console.log("ilość obiektów: " + itemsCount);
+									console.log("ilość updateów: " + updatesCount);
+									console.log("   ilość tagów: " + tagsCount);
+									console.log(" różnych tagów: " + diffTags);
+								}
+							}, 10000);
+					}
+					else{
+						itemsCount++;
+
+						if(item.Tags.constructor === String){
+							// console.log("id: " + item.Id + " tags: " + item.Tags );
+							//rozdzielamy string do tablicy
+							var tagsSplited = item.Tags.split(" "); 
+							tagsCount += tagsSplited.length;
+							//zliczanie różnych tagów
+							for(var i=0; i < tagsSplited.length; i++){
+								if(tags[tagsSplited[i]] === undefined){
+									tags[tagsSplited[i]] = 1; //cokolwiek byle pole było defined
+									diffTags++;
+								}
+								// else{
+								// 	tags[tagsSplited[i]]++;
+								// }
+							}
+							//zamiana stringa na tablicę w bazie
+							coll.update({Id: item.Id}, {$set: {Tags: tagsSplited}}, function(err){
+								if(err) { console.log(err); }
+								else{
+									updatedCount++; //liczymy wykonane update-y
+								}
+							});
+							updatesCount++; //liczymy ilość update-ów do wykonania
+						}
+
+						if(itemsCount % 1000 === 0){
+							console.log("obiektów: " + itemsCount + " aktualizacji: " + updatesCount +
+						 	" tagów: " + tagsCount + " różnych tagów: " + diffTags);
+						}
+					}
+				});
+			}
+		});
+	}
 });
