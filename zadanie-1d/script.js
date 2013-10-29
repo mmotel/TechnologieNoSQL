38e710df635febf89204c01e16cb332106f2d1c3
mmotel <mateuszmotel@gmail.com>
2013-10-29 22:45:22 +0100

    dodałem skrypt do zadania 1d

---
+++ zadanie-1d/script.js
@@ -0,0 +1,24 @@
+// run: mongo text script.js
+// set or comment limit to set number of elements
+
+var coll = db.text;
+
+var result = coll.aggregate(
+  { $group: {_id: "$word", count: {$sum: 1}} },
+  { $sort: {count: -1} }//,
+  // { $limit: 1000 } 
+);
+
+// printjson(result);
+
+var totalCount = 0;
+
+for(var i = 0; i < result.result.length; i++){
+	totalCount += result.result[i].count;
+}
+
+var wordsCount = coll.count();
+
+print(" słów: " + i);
+print("ilość: " + totalCount);
+print("część: " + (totalCount / wordsCount) * 100 + "%");
