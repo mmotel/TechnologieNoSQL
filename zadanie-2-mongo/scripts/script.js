fa4f3937280fa9712c0b497d70c9fd703c69e88c
Mateusz Motel <mateuszmotel@gmail.com>
2013-12-16 09:00:55 +0100

    Update script.js

--- zadanie-2-mongo/scripts/script.js
+++ zadanie-2-mongo/scripts/script.js
@@ -12,17 +12,3 @@ var result = coll.aggregate(
 );
 
 printjson(result);
-
-// var totalCount = 0;
-
-// for(var i = 0; i < result.result.length; i++){
-//  print(result.result[i]["_id"] + ": " + result.result[i].count);
-// }
-
-// var wordsCount = coll.count();
-
-// print(" słów: " + i);
-// print("ilość: " + totalCount);
-// print("część: " + (totalCount / wordsCount) * 100 + "%");
-
-// "_id": 0, "comment": 0, "modelName": 0, "displayName": 0, "title": 1, "timestamp": 0, "image": 0, "userId": 0, "private": 0, "director": 0, "source": 0, "version": 0, "link": 0, "lastModified": 0, "action": 0, "lctitle": 0, "objectKey": 0 
